09efc925648248334497c09ed228bba2399a7b29
Alphactory <aedan.e.taylor@gmail.com>
2020-12-25 22:26:12 -0500

    completed day 6

--- Day6/main.py
+++ Day6/main.py
@@ -4,15 +4,41 @@ for line in open("data"):
 
 def part1():
     sum = 0
-    for grouping in datastring.split("\n\n"):
-        print(grouping)
-        grouping.strip()
-        chars = []
-        for char in grouping:
-            if char not in chars and char != "\n":
-                chars.append(char)
-        sum += len(chars)
-
+    groups = datastring.split("\n\n")
+    for group in groups:
+        forms = group.split("\n")
+        distinct_chars = []
+        for form in forms:
+            for char in form:
+                if char not in distinct_chars:
+                    distinct_chars.append(char)
+        sum += len(distinct_chars)
     print(sum)
 
+def part2():
+    sum = 0
+    groups = datastring.split("\n\n")
+    for group in groups:
+        # build up a list of distinct characters in a group
+        forms = group.split("\n")
+        distinct_chars = []
+        for form in forms:
+            for char in form:
+                if char not in distinct_chars and char:
+                    distinct_chars.append(char)
+        shared_chars = []
+        # check which of the distinct characters are shared by all forms in the group
+        for char_to_check in distinct_chars:
+            flag = True
+            for form in forms:
+                if form:
+                    if char_to_check not in form:
+                        flag = False
+            if flag:
+                shared_chars.append(char_to_check)
+        sum += len(shared_chars)
+    print(sum)
+
+
 part1()
+part2()
